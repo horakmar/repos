8ac1e88144d067829d68a9c9cae091c2b5de0500
Martin Horak <horak.martin@gmail.com>
2020-10-30 08:54:21 +0100

    Reformat in compact style.

--- c#/getparam.cs
+++ c#/getparam.cs
@@ -6,24 +6,17 @@ using System;
 using System.Text;
 using System.Collections.Generic;
 
-public class Example
-{
-    public static void Main(string[] args)
-    {
+public class Example {
+    public static void Main(string[] args) {
         // GetParam -------------------------------
         var parms = new List<string>();
         int verbose = 1;
         string configFile = "";
-        try
-        {
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i].StartsWith("-"))
-                {
-                    foreach (char c in args[i].Substring(1))
-                    {
-                        switch (c)
-                        {
+        try {
+            for (int i = 0; i < args.Length; i++) {
+                if (args[i].StartsWith("-")) {
+                    foreach (char c in args[i].Substring(1)) {
+                        switch (c) {
                             case 'v':
                                 verbose++;
                                 break;
@@ -36,15 +29,11 @@ public class Example
                                 break;
                         }
                     }
-                }
-                else
-                {
+                } else {
                     parms.Add(args[i]);
                 }
             }
-        }
-        catch (IndexOutOfRangeException)
-        {
+        } catch (IndexOutOfRangeException) {
             Console.WriteLine("Parameter read error.");
             return;
         }
@@ -52,11 +41,9 @@ public class Example
         // Getparam end ----------------------------
 
         Console.WriteLine("Verbose: {0}\nConfig: {1}", verbose, configFile);
-        if (parms.Count > 0)
-        {
+        if (parms.Count > 0) {
             Console.Write("Parameters: ");
-            foreach (string p in parms)
-            {
+            foreach (string p in parms) {
                 Console.Write(p + " ");
             }
         }
